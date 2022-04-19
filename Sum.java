1430f8d3658b5e71b04a2413c4178dda24dea301
Ilya Cheshkov <0921408@gmail.com>
2022-04-19 11:48:12 +0300

    Create Sum.java

---
+++ Sum.java
@@ -0,0 +1,19 @@
+/**
+*I'm new to coding and now I want to get the sum of two arrays...actually the sum of all their elements. I'll appreciate for your help.
+*P.S. Each array includes only integer numbers. Output is a number too.
+*/
+
+
+public class Sum {
+
+  public static int arrayPlusArray(int[] arr1, int[] arr2) {
+    int sum = 0;
+    for (int i = 0; i < arr1.length; i ++) {
+      sum += arr1[i];
+    }
+    for (int i = 0; i < arr2.length; i ++) {
+      sum += arr2[i];
+    }
+    return sum;
+  }
+}
