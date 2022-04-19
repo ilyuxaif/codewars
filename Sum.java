997451d3947ebd48630941fab63d2986a172f4ed
Ilya Cheshkov <0921408@gmail.com>
2022-04-19 12:04:24 +0300

    Update Sum.java

--- Sum.java
+++ Sum.java
@@ -1,6 +1,7 @@
 /**
 *I'm new to coding and now I want to get the sum of two arrays...actually the sum of all their elements. I'll appreciate for your help.
 *P.S. Each array includes only integer numbers. Output is a number too.
+*https://www.codewars.com/kata/57eadb7ecd143f4c9c0000a3/train/java
 */
 
 
